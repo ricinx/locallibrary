a1160b09d7af5043821c03a3c96d000279d3e415
ricinx <jiae@hotmail.com>
2018-04-20 16:30:41 +0800

    session

--- catalog/views.py
+++ catalog/views.py
@@ -14,16 +14,20 @@ def index(request):
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()  # The 'all()' is implied by default.
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
-        'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        'catalog/index.html',
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors,
+                    'num_visits':num_visits},
     )
 
 class BookListView(generic.ListView):
     model = Book
-
+    paginate_by = 10
     #def get_queryset(self):
         #return Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
 
