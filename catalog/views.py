56a59f12db886212537d68c26233ea32e92995cd
ricinx <jiae@hotmail.com>
2018-05-04 16:54:30 +0800

    author create

--- catalog/views.py
+++ catalog/views.py
@@ -11,6 +11,10 @@ from django.contrib.auth.decorators import permission_required
 
 from .forms import RenewBookForm
 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Author
+
 # Create your views here.
 def index(request):
     """
@@ -86,3 +90,16 @@ def renew_book_librarian(request, pk):
         form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
 
     return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst':book_inst})
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = '__all__'
+    initial={'date_of_death':'05/01/2018',}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name','last_name','date_of_birth','date_of_death']
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
